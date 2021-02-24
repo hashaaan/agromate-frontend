61b0bef95e46785ae6f3a9392b216a719776288e
hashaaan <shalithavidanapathirana@gmail.com>
2021-02-24 19:41:59 +0530

    Add ui packages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./custom.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Agriculture Field Support and Guidance System</p>
+        <div className="App-link">Agromate - Sri Lanka</div>
       </header>
     </div>
   );
